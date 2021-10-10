529d5712303032092048fe586f70f4f76e0c4968
SawitriFK <sawitrifinak@gmail.com>
2021-10-10 03:50:19 +0100

    Solution problem 5

---
+++ Assets/Script/Movement/MouseMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMovement : MonoBehaviour
+{
+    private float moveSpeed = 5;
+    private bool isMoving = false;
+
+    private Vector3 targetPosition;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            SetTargetPosition();
+        }
+        if (isMoving)
+        {
+            Move();
+        }
+    }
+
+    void SetTargetPosition()
+    {
+        targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        targetPosition.z = transform.position.z;
+
+        isMoving = true;
+    }
+
+    void Move()
+    {
+        // lingkaran dengan kecepatan tertentu mengikuti kursor mouse / letak touch
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, targetPosition);
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        if (transform.position == targetPosition)
+        {
+            isMoving = false;
+        }
+    }
+}
