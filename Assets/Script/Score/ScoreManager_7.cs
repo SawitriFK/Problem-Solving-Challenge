b03ff55ab6e7683f76a9d9276fecd1cb457ee0ee
SawitriFK <sawitrifinak@gmail.com>
2021-10-10 05:01:11 +0100

    Solution problem 7

---
+++ Assets/Script/Score/ScoreManager_7.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager_7 : MonoBehaviour
+{
+    public static int score;
+    public Text _score;
+
+    private void OnEnable()
+    {
+        score = 0;
+    }
+
+    public void addScore(int amount)
+    {
+        score += amount;
+        _score.text = score.ToString();
+
+    }
+
+}
---
+++ Assets/Script/Touch/KillBox.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillBox : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "box")
+        {
+
+            Destroy(collision.gameObject);
+            // For add score
+            AddScore();
+        }
+
+    }
+    private void AddScore()
+    {
+        if (FindObjectOfType<ScoreManager_7>())
+        {
+            Debug.Log("Masuk");
+            FindObjectOfType<ScoreManager_7>().addScore(1);
+        }
+    }
+}
