9d4e62c2c697c00da2312f79f70d99bb8a314184
SawitriFK <sawitrifinak@gmail.com>
2021-10-10 03:40:19 +0100

    Solution problem 4

---
+++ Assets/Script/Movement/KeyMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyMovement : MonoBehaviour
+{
+    private float moveSpeed = 5;
+
+    private Rigidbody2D Rigidbody2D;
+
+    private void Awake()
+    {
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        //lingkaran bergerak dengan input atas bawah kiri kanan
+        float xInput = Input.GetAxis("Horizontal");
+        float yInput = Input.GetAxis("Vertical");
+        Vector2 direction = new Vector2(xInput, yInput).normalized;
+
+        Rigidbody2D.velocity = direction * moveSpeed;
+    }
+}
