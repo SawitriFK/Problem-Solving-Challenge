832f01758f518fbd69aa4b71cd12d7b5ac7454ff
SawitriFK <sawitrifinak@gmail.com>
2021-10-10 03:11:19 +0100

    Solution problem 2

---
+++ Assets/Script/Movement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody2D rigid2d;
+    void Start()
+    {
+        //lingkaran bergerak dengan suatu kecepatan konstan sehingga keluar dari layar
+        rigid2d = GetComponent<Rigidbody2D>();
+        rigid2d.velocity = new Vector2(2f, 2f);
+    }
+}
